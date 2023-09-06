f404013daff32398942f3a8b53a8db419628175b
martinyegros <martinyegros1@gmail.com>
2023-09-06 20:49:46 -0300

    Desafío finalizado

--- ProductManager.js
+++ ProductManager.js
@@ -1,20 +1,21 @@
 
 class ProductManager {
+    //Array vacío
     constructor() {
         this.products = [];
     }
-
+    //Método getProducts
     getProducts = () => {
         return this.products;
     }
-
+    //Método addProduct
     addProduct = ( title, description, price, thumbnail, code, stock ) => {
         //Validar campos obligatorios
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.log('Todos los campos son obligatorios')
             return;
         }
-
+        //Producto gestionado
         const product =
             {
                 title,
@@ -24,15 +25,12 @@ class ProductManager {
                 code,
                 stock
             }
-        //Validar que no se repita el campo code (ÉSTA VALIDACIÓN NO PUDE LOGRARLA)
-        /* const productIndex = this.products.findIndex(product => product.code);
-        const campoCode = this.products[productIndex].includes(product.code);
-        
-        if (campoCode) {
+        //Validar que no se repita el campo code
+        if (this.products.some(product => product.code === code)) {
             console.log('Código ya existente');
             return;
-        } */
-
+        }
+        //Creación del id
         if (this.products.length === 0) {
             product.id = 1;
         } else {
@@ -42,7 +40,7 @@ class ProductManager {
         this.products.push(product);
 
     }
-
+    //Método getProductById
     getProductById = (idProduct) => {
         const productId = this.products.find(product => product.id === idProduct);
 
@@ -54,17 +52,21 @@ class ProductManager {
         }
     }
 }
-
+//Creación de la instancia
 const manejadorEventos = new ProductManager();
-
+//Llamado de getProducts
 console.log(manejadorEventos.getProducts());
-
+//Llamado de addProduct
 manejadorEventos.addProduct('Turmalina Negra', 'La turmalina negra es un mineral empleado para evitar que las influencias negativas de un lugar o individuo, logren afectar al portador o a su entorno', 900, 'https://www.gemascanarias.com/img/cms/turmalina-negra-en-bruto.jpg', 'CODE-001', 50)
+//Id generado sin repetirse
 manejadorEventos.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
+//Error porque el código se repite
 manejadorEventos.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
-
+//Llamado de getProducts
 console.log(manejadorEventos.getProducts());
-
+//Evaluación de getProductById
+//Error porque no se encuentra el producto
 console.log(manejadorEventos.getProductById(5));
+//El producto encontrado
 console.log(manejadorEventos.getProductById(1));
 
