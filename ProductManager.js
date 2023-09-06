edcd3f9c1d9949a79ef86d0a63dedb2130f6b9fd
martinyegros <martinyegros1@gmail.com>
2023-09-06 03:10:16 -0300

    Desafío incompleto

--- ProductManager.js
+++ ProductManager.js
@@ -14,31 +14,33 @@ class ProductManager {
             console.log('Todos los campos son obligatorios')
             return;
         }
-        
-        const product = [
-            {
-                title: "Aceitunas descarozadas",
-                description: "Aceitunas verdes descarozadas orgánicas 'San Nicolas' 900g",
-                price: 900,
-                thumbnail: 'https://cdn.newgarden.com.ar/media/catalog/product/cache/dda7253a1a2f6711745de410175d10f8/a/c/aceitunas-verdes-descarozadas-organicas-san-nicolas-x-250-g.jpg',
-                code: 'PROD-001',
-                stock: 25
-            }
-        ]
 
-        const campoCode = this.products.includes(product.code);
+        const product =
+            {
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock
+            }
+        //Validar que no se repita el campo code (ÉSTA VALIDACIÓN NO PUDE LOGRARLA)
+        /* const productIndex = this.products.findIndex(product => product.code);
+        const campoCode = this.products[productIndex].includes(product.code);
+        
         if (campoCode) {
-            console.log('Codigo ya existente');
+            console.log('Código ya existente');
             return;
-        }
+        } */
 
         if (this.products.length === 0) {
             product.id = 1;
         } else {
-            product.id = this.products[this.products.lenght - 1].id + 1;
+            product.id = this.products[this.products.length - 1].id + 1;
         }
 
-        this.products.push(product)
+        this.products.push(product);
+
     }
 
     getProductById = (idProduct) => {
@@ -47,6 +49,22 @@ class ProductManager {
         if (!productId) {
             console.log('Producto no encontrado');
             return;
+        } else {
+            console.log(productId);
         }
     }
 }
+
+const manejadorEventos = new ProductManager();
+
+console.log(manejadorEventos.getProducts());
+
+manejadorEventos.addProduct('Turmalina Negra', 'La turmalina negra es un mineral empleado para evitar que las influencias negativas de un lugar o individuo, logren afectar al portador o a su entorno', 900, 'https://www.gemascanarias.com/img/cms/turmalina-negra-en-bruto.jpg', 'CODE-001', 50)
+manejadorEventos.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
+manejadorEventos.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
+
+console.log(manejadorEventos.getProducts());
+
+console.log(manejadorEventos.getProductById(5));
+console.log(manejadorEventos.getProductById(1));
+
